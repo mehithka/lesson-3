4ebc6d6679852d90ae8b4f3ab55330de3a371630
mehithka <mehith.karacasia@gmail.com>
2024-12-09 19:55:24 +0500

    lesson 3 data type

---
+++ data types.py
@@ -0,0 +1,30 @@
+name = "mehith"
+age = 13
+weight = 56.6
+Is_student = True
+
+#printing
+print("Name is: " ,  name)
+print (type(name))
+
+print("His age is: ", age )
+print(type(age))
+
+print("his weight is : ",weight)
+print(type(weight))
+
+print("he is a student" , Is_student)
+print(type(Is_student))
+
+#type casting
+
+print("after type casting the data type is :", "\n")
+
+age = str(age)
+print(age)
+print("data type of age is: ",type(age))
+
+
+weight = int (weight)
+print("his weight is : ",weight)
+print(type(weight))
