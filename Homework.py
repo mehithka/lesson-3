5c3c7068052624001163e70f86868f04eec1fb53
mehithka <mehith.karacasia@gmail.com>
2024-12-09 20:10:03 +0500

    Home work lesson 3

---
+++ Homework.py
@@ -0,0 +1,10 @@
+text = "hElLo"
+print("original text: " ,text)
+print("uppercase",str.upper(text))
+print("lowercase" , str.lower(text))
+
+
+strone = input("enter a string")
+strtwo = input("enter a string")
+strthree = strone+" "+strtwo 
+print("\n", "the joined strings are: " , strthree)
